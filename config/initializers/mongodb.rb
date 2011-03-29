e9cd1d7a40d70b235638f174b8d2e52b8743541c
Edijs Petersons <eddpeterson@gmail.com>
2011-03-28 21:16:36 -0400

    Update configs for MongoHQ on Heroku

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,7 +1,12 @@
-db_config = YAML::load(File.read(Rails.root.to_s + "/config/database.yml"))
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
+else
+  db_config = YAML::load(File.read(Rails.root.to_s + "/config/database.yml"))
 
-if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
-  mongo = db_config[Rails.env]
-  MongoMapper.connection = Mongo::Connection.new(mongo['hostname'])
-  MongoMapper.database = mongo['database']
+  if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
+    mongo = db_config[Rails.env]
+    MongoMapper.connection = Mongo::Connection.new(mongo['hostname'])
+    MongoMapper.database = mongo['database']
+  end
 end
